97a409c1a4dbae7eddbec68eb43c04d2e4651101
Baranov Pavel <pavel-info@ya.ru>
2023-02-26 21:28:30 +0300

    jwt

--- library/urls.py
+++ library/urls.py
@@ -4,6 +4,11 @@ from django.urls import path, include
 from authors.views import AuthorModelViewSet, BookModelViewSet, BiographyModelViewSet, ArticleModelViewSet, ProjectModelViewSet, TaskModelViewSet, MyAPIView
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 router = DefaultRouter()
 router.register(r'authors', AuthorModelViewSet)
@@ -24,4 +29,8 @@ urlpatterns = [
     path('myapi/', include(myrouter.urls)),
     path('myproject/', ProjectModelViewSet.as_view({'get': 'list'})),
     path('api-token-auth/', views.obtain_auth_token),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
 ]
